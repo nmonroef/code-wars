7977be298a517a52f7110eef2359d5f207b0aed7
Nathan Monroe <nmonroef@gmail.com>
2024-05-20 00:49:26 -0500

    added Fizzbuz

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
-class Anamils {
-  constructor(name) {
-    this.name = name
-  }
-  speak() {
-    console.log("spek")
-  }
-}
+//Write a program that prints the numbers from 1 to n. But for multiples of three, print 'Fizz' instead of the number,
+ //and for the multiples of five, print 'Buzz'. For numbers that are multiples of both three and five, print 'FizzBuzz'."
 
-class dog extends Anamils {
-  constructor(name,breed,size) {
-    super(name)
-    this.breed = breed
-    this.size = size
-  }
-  speak() {
-    console.log("speek speek")
-  }
-}
-
-class cat extends Anamils {
-  constructor(name, breed,) {
-    super(name)
-    this.breed = breed
+function fizzbuzz(number) {
+  for(let i = 0; i <= number; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz")
+    }else if (i % 3 == 0) {
+      console.log("Fizz")
+    }else if (i % 5 == 0) {
+      console.log("Buzz")
+    }else {
+      console.log(i)
+    }
   }
 }
- let nathan = new cat("Nathan","guy")
+fizzbuzz(15)
 
